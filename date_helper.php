c2034181533566de37b9a1a5f0b1e678af6b6fb7
aahadr <31738092+aahadr@users.noreply.github.com>
2019-10-14 19:41:51 +0700

    Update date_helper.php

--- date_helper.php
+++ date_helper.php
@@ -5,20 +5,21 @@
 	
 		/**
 		 * checkdate — Validate a Gregorian date
-		Parameters :
-		month
-		The month is between 1 and 12 inclusive.
+		 * Parameters :
+		 * month
+		 * The month is between 1 and 12 inclusive.
 
-		day
-		The day is within the allowed number of days for the given month. Leap years are taken into consideration.
+		 * day
+		 * The day is within the allowed number of days for the given month. Leap years are taken into consideration.
 
-		year
-		The year is between 1 and 32767 inclusive.
+		 * year
+		 * The year is between 1 and 32767 inclusive.
 
 		 *
 		 * @return void
 		 * @author Ayatulloh Ahad R - ayatulloh@idprogrammer.com
 		 **/
+		
 		
 		function validateDate($date, $format = 'Y-m-d H:i:s')
 		{
